7ae531f02d5b14b9c175704616d9fa5a472c86d4
Flaviog <grammaticof3@gmail.com>
2025-05-30 17:03:52 +0200

    Commit iniziale: ignoro venv, __pycache__, .DS_Store

---
+++ app.py
@@ -0,0 +1,22 @@
+# Import standards
+from flask import Flask, render_template
+import sqlite3 as sq3
+
+# Database connection (conn.Commit, conn.Close, conn.cursor())
+conn = sq3.connect("db_name") # Se "db_name" non è un file .sql verrà creato da SQLite
+cursor = conn.cursor()	# anche il cursore va chiuso
+
+# Es:
+# sql = "SELECT * FROM PERSONE WHERE nome = (?)"
+# cursor.execute(sql, ("Gianni", )) # si mette sempre la ',' per far riconoscere che è una tupla -> trailing
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+cursor.close()
+conn.close()
