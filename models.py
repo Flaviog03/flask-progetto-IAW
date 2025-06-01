85728988461a058b829ff23ef8e1b81d9d5dc3fd
Flaviog <grammaticof3@gmail.com>
2025-06-01 16:26:18 +0200

    Feat: aggiunta modello User per la gestione degli utenti e aggiornato navbar per il profilo

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_login import UserMixin, LoginManager
+import dao
+
+class User(UserMixin):
+    def __init__(self, id, password, email, nome, cognome, ruolo):
+        self.id = id
+        self.password = password
+        self.email = email
+        self.nome = nome
+        self.cognome = cognome
+        self.ruolo = ruolo
